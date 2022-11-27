7af08a6aa2d6e4c78717dae566ec5c347d4cc2ef
Bruce Becker <brucellino@protonmail.com>
2022-11-27 09:16:32 +0100

    test(config): add failing test for configuration directory
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- test/test_vault_cluster.py
+++ test/test_vault_cluster.py
@@ -1,4 +1,16 @@
 def test_vault_user(host):
-  assert host.group("vault").exists
-  assert host.user("vault").exists
-  assert "vault" in host.user("vault").groups
+    assert host.group("vault").exists
+    assert host.user("vault").exists
+    assert "vault" in host.user("vault").groups
+
+
+def test_vault_config_dir(host):
+    """
+    Ensure that the vault configuration directory is present and properly configured
+    """
+    vault_config_dir = host.file("/etc/vault.d")
+    assert vault_config_dir.exists
+    assert vault_config_dir.is_directory
+    assert vault_config_dir.user == "vault"
+    assert vault_config_dir.group == "vault"
+    assert vault_config_dir.mode == "0o644"
